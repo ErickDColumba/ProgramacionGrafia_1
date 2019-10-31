6be393c7822ff89a299f63a73024a2f60143c053
ErickDColumba <55849689+ErickDColumba@users.noreply.github.com>
2019-10-31 01:38:32 -0500

    Trarea 1 Repaso de Arreglos y Matrices

--- src/colecionesylistas/MetGenerales.java
+++ src/colecionesylistas/MetGenerales.java
@@ -5,7 +5,10 @@
  */
 package colecionesylistas;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  *
@@ -13,13 +16,75 @@ import java.util.List;
  */
 public class MetGenerales {
  
+    Scanner leer = new Scanner(System.in);
+    public void imListSt (List a ){
+        System.out.println("La lista es ");
+        Iterator it=  a.iterator();
+        while (it.hasNext()){
+        String elemento = (String) it.next();
+            System.out.println(""+elemento);   
+        }    
+    }
     
-    public void imList (List lista){
+    public List NoRamdom(int n){
+        
+        List<Integer> lista = new ArrayList <>();
+        
+        for (int i=0 ; i <n;i++){
+        System.out.println("Ingrese dato"+(i+1));
+        Integer valor = leer.nextInt();
+            lista.add(valor);
+        }
+        
+        return lista;
     
-        System.out.println(""+lista);
+    }
+    
+    public List Ordenada(int n){
         
+        List<Integer> lista = new ArrayList <>();
+        int a=1;
+        for (int i=0 ; i <n;i++){
+        Integer valor = a;
+            lista.add(valor);
+            a++;
+        }
         
+        return lista;
+    
     }
     
     
+    public Integer sumavec (List l){
+    
+    Integer suma=0;
+        Iterator it=  l.iterator();
+        while (it.hasNext()){
+        Integer tem = (Integer)it.next();
+        suma+=tem;
+        }    
+        
+        return suma;
+    }
+    
+    
+  public  boolean esPrimo(int numero){
+  int contador = 2;
+  boolean primo=true;
+ 
+  if(numero!=1){
+  while ((primo) && (contador!=numero)){
+    if (numero % contador == 0)
+      primo = false;
+    contador++;
+  }
+  
+  }
+  return primo;
 }
+    
+    
+    
+    
+    
+}//fin
