229ed08314a346a6a6be327b0eef90e1de19387a
ErickDColumba <55849689+ErickDColumba@users.noreply.github.com>
2019-10-31 00:04:35 -0500

    Trarea 1 Repaso de Arreglos y Matrices

---
+++ src/colecionesylistas/ColecionesYListas.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package colecionesylistas;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author EDCP
+ */
+public class ColecionesYListas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Scanner leer= new Scanner (System.in);  
+        Ejercicios a = new Ejercicios();
+        System.out.println("Ingrese ejercicio que desea Ejecutar ");
+        int op=0;
+        op = leer.nextInt();
+        switch (op){
+            case 1:
+                  a.eje1();
+            break;
+             case 2:
+                
+            break;
+             case 3:
+                  
+            break;
+             case 4:
+                
+            break;
+             case 5:
+                 
+            break;
+            case 6:
+                  
+            break;
+            case 7:
+                  
+            break;
+            case 8:
+               
+            break;
+            case 9:
+                  
+            break;
+             case 10:
+                 
+            break;
+            case 11:
+                
+            break;
+            case 12:
+                 
+            break;
+           
+    }
+    
+    }
+}//fin
